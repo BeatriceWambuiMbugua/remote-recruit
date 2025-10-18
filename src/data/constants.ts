19337638256f1a0085a1cb0e6b81dfd11668606d
BeatriceWambuiMbugua <beatricewambuimbugua@gmail.com>
2025-10-19 00:03:38 +0300

    Add developer skills constants and interface for improved skill management

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -7,3 +7,20 @@ export const premiumPlanFeatures = [
     { id: 6, text: "Unlimited invites to jobseekers" },
   ];
   
+
+
+export interface Skill {
+    id: number;
+    name: string;
+    isMore?: boolean; // flag for "+12"
+  }
+  
+  export const developerSkills: Skill[] = [
+    { id: 1, name: "Python Dev" },
+    { id: 2, name: "Javascript" },
+    { id: 3, name: "Front End" },
+    { id: 4, name: "Back End" },
+    { id: 5, name: "IOS Development" },
+    { id: 6, name: "+12", isMore: true },
+  ];
+  
