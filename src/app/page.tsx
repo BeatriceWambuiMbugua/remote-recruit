e56eb0675e1a82540b449a56fe4eec9632a5db31
BeatriceWambuiMbugua <beatricewambuimbugua@gmail.com>
2025-10-18 17:10:36 +0300

    Fix minor layout issue by adjusting whitespace in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col ">
       <Navbar />
       <HeroSection />
     </div>
