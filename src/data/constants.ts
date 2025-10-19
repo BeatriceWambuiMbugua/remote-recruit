a6b470cc9ff27e7925e95d8b6e00f997b590e539
BeatriceWambuiMbugua <beatricewambuimbugua@gmail.com>
2025-10-19 19:05:34 +0300

    Update FAQ answer formatting in constants.ts for consistency

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -157,7 +157,7 @@ export interface Skill {
     },
     {
       question: "What are the pricing?",
-      answer: "Actually beard single-origin coffee, twee 90's PBR Echo Park sartorial try-hard freegan Portland ennui. Selvage jean shorts 90's, Vice American Apparel try-hard food truck Shoreditch fap lomo Wes Anderson. Art party Thundercats High Life Tumblr", // This can be empty if clicking opens a modal or expands more FAQs
+      answer: "Actually beard single-origin coffee, twee 90's PBR Echo Park sartorial try-hard freegan Portland ennui. Selvage jean shorts 90's, Vice American Apparel try-hard food truck Shoreditch fap lomo Wes Anderson. Art party Thundercats High Life Tumblr", 
       isViewMore: true, // optional flag to handle special behavior
     },
   ];
